8a77afb8ece76d0f06a228a70b09e92d3f1327c3
hamza <hamza.benketaf@gmail.com>
2026-08-08 23:04:56 +0200

    css

--- src/App.tsx
+++ src/App.tsx
@@ -117,7 +117,7 @@ function App() {
           }}
         />
         <FaArrowAltCircleRight
-          color={allNamesFilled ? 'chartreuse' : 'gray'}
+          color={allNamesFilled ? 'greenyellow' : 'gray'}
           size={40}
           className={allNamesFilled ? style.btn : style.disabled}
           onClick={() => {
