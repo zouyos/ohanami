362ab8fe507928cb9746db299942d10ba1ac43a5
hamza <hamza.benketaf@gmail.com>
2026-08-09 14:31:24 +0200

    fixing background issues on iOS

--- src/App.tsx
+++ src/App.tsx
@@ -318,9 +318,11 @@ function App() {
 
   return (
     <div className={style.app}>
-      <h1 className={style.title}>おはなみ</h1>
-      <div className={style.content}>
-        {showGame ? gameDiv : nbPlayers ? nameDiv : nbPlayersDiv}
+      <div className={style.background}>
+        <h1 className={style.title}>おはなみ</h1>
+        <div className={style.content}>
+          {showGame ? gameDiv : nbPlayers ? nameDiv : nbPlayersDiv}
+        </div>
       </div>
     </div>
   );
